d7f1ac4a7eb0bd6973d3df2095412ea8b0d4e9ea
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-01-02 14:28:33 -0800

    Added Flask session and new route from start.html to /start

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, flash, render_template
+from flask import Flask, request, redirect, flash, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -8,22 +8,27 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
 
 @app.route('/')
-def start_page():
+def home_page():
   """Show start page with survey title and instructions"""
-  responses.clear()
   title = survey.title
   instructions = survey.instructions
 
   return render_template('start.html', title=title, instructions=instructions)
 
+@app.route('/start', methods = ['POST'])
+def start_page():
+  session['responses'] = []
+  return redirect ('/questions/0')
+
 @app.route('/answer', methods = ['POST'])
 def handle_question():
-  """Save answer to list and redirect to next question."""
+  """Save answer to flask session and redirect to next question."""
   answer = request.form['answer']
+  responses = session['responses']
   responses.append(answer)
+  session['responses'] = responses
 
   if(len(responses) == len(survey.questions)):
     return redirect('/complete')
@@ -35,6 +40,7 @@ def show_question(question_id):
   """Display current question."""
   question = survey.questions[question_id]
   choices = survey.questions[question_id].choices
+  responses = session.get('responses')
 
   if(responses is False):
     return redirect('/')
